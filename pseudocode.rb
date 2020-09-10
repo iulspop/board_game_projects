5c3ffa584b1123c0415d8ffc0208ae9672e73a39
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-10 09:17:35 -0400

    write informal pseudocode for problem 1

---
+++ pseudocode.rb
@@ -0,0 +1,17 @@
+=begin
+write out pseudo-code (both casual and formal) that does the following:
+
+    a method that returns the sum of two integers
+    a method that takes an array of strings, and returns a string that is all those strings concatenated together
+    a method that takes an array of integers, and returns a new array with every other element
+=end
+
+=begin
+== Problem 1 ==
+Given two integers
+add them together
+return the result
+
+
+
+=end
