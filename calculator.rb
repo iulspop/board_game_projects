94542659c699ea4b69f251e19e1f0e4789f539f2
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-10 10:41:18 -0400

    run rubocop

--- calculator.rb
+++ calculator.rb
@@ -2,16 +2,16 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-prompt("Simple Arithmetic Calculor")
+prompt('Simple Arithmetic Calculor')
 
-operation = ""
+operation = ''
 loop do
   prompt("What operation would you like to perfom?
    1) add 2) substract 3) multiply 4) divide")
   operation = gets.chomp
 
-  if ['1', '2', '3', '4'].include?(operation) then break
-  else prompt("Invalid input. Please enter either 1, 2, 3 or 4.") end
+  if %w[1 2 3 4].include?(operation) then break
+  else prompt('Invalid input. Please enter either 1, 2, 3 or 4.') end
 end
 
 prompt("What's the first number?")
@@ -21,14 +21,15 @@ num2 = 0
 loop do
   prompt("What's the second number?")
   num2 = gets.chomp.to_i
-  next prompt("Cannot divide by 0") if operation == "divide" && num2 == 0
+  next prompt('Cannot divide by 0') if operation == 'divide' && num2.zero?
+
   break
 end
 
 result = case operation
-         when "1" then num1 + num2
-         when "2" then num1 - num2
-         when "3" then num1 * num2
-         when "4" then num1 / num2 end
+         when '1' then num1 + num2
+         when '2' then num1 - num2
+         when '3' then num1 * num2
+         when '4' then num1 / num2 end
 
 prompt("The result is: #{result}")
