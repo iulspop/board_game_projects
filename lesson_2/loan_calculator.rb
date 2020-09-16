fa120805483709bfc79da7b2c6bae83ad3461caa
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-16 09:22:07 -0400

    go back to arguments on two lines

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -154,9 +154,13 @@ loop do
   months          = get_loan_duration_in_months()
   monthly_rate    = apr_to_monthly_rate_as_decimal(get_apr)
 
-  monthly_payment = calculate_monthly_payment(loan_amount, months, monthly_rate).round(2)
-  total_interest  = calculate_total_interest(loan_amount, months, monthly_rate, monthly_payment).round(2)
-  total_payment   = calculate_total_payment(loan_amount, total_interest).round(2)
+  monthly_payment = calculate_monthly_payment(loan_amount,
+                                              months, monthly_rate).round(2)
+
+  total_interest = calculate_total_interest(loan_amount, months, monthly_rate,
+                                            monthly_payment).round(2)
+
+  total_payment = calculate_total_payment(loan_amount, total_interest).round(2)
 
   display_loan_results(monthly_payment, months, total_interest, total_payment)
   break unless again?
