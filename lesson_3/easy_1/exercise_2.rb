44bcc7eda63e2486c47ab493211b6e20b152040f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-18 11:21:59 -0400

    answer questions exercise 2

---
+++ lesson_3/easy_1/exercise_2.rb
@@ -0,0 +1,19 @@
+# Describe the difference between ! and ? in Ruby.
+# And explain what would happen in the following scenarios:
+
+# what is != and where should you use it?
+
+# put ! before something, like !user_name
+# not operator
+
+# put ! after something, like words.uniq!
+# part of method name, usualy indicating mutating method
+
+# put ? before something
+# ternary operator
+
+# put ? after something
+# method name, often returns booleans
+
+# put !! before something, like !!user_name
+# double NOT
