4e25c655d4f8fd8fe57577e9693105d15e05a5a5
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-10-01 12:47:25 -0400

    move contants to file

--- lesson_6/tic_tac_toe/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe/tic_tac_toe.rb
@@ -1,36 +1,7 @@
 require 'io/console'
 load 'ascii_art.rb'
+load 'constants.rb'
 load 'methods.rb'
-
-VALID_MOVES = {
-  'q' => [0, 0],
-  'w' => [0, 1],
-  'e' => [0, 2],
-  'a' => [1, 0],
-  's' => [1, 1],
-  'd' => [1, 2],
-  'z' => [2, 0],
-  'x' => [2, 1],
-  'c' => [2, 2]
-}
-
-WIN_SCORE = 1
-
-RULES_MESSAGE = <<-MSG
-The rules are: 
-    Two players, X and O, take turns marking the spaces in a 3×3 grid. 
-
-    The player who succeeds in placing three of their marks
-    in a horizontal, vertical, or diagonal row wins the round.
-
-    The first to win three rounds wins the game!
-MSG
-
-MOVES_DISPLAY = <<-MSG
-q w e
-a s d
-z x c
-MSG
 
 welcome
 loop do
