16bbaf38a4dce3faae58c5217df612e90c6f3a81
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-10-05 18:58:03 -0400

    add calc_totals methods

--- lesson_6/twenty_one/twenty_one.rb
+++ lesson_6/twenty_one/twenty_one.rb
@@ -22,7 +22,7 @@ def draw_card(deck, hand)
 end
 
 def get_hands(deck)
-  hands = {player: [], dealer: []}
+  hands = { player: [], dealer: [] }
   2.times do
     draw_card(deck, hands[:player])
     draw_card(deck, hands[:dealer])
@@ -30,11 +30,51 @@ def get_hands(deck)
   hands
 end
 
+def calc_ace(total, hand)
+  if total + 11 <= 21
+    11
+  else
+    1
+  end
+end
+
+def calc_hand(hand)
+  total = 0
+
+  hand.each do |(value, _)|
+    if "Ace" == value
+      total += calc_ace(total, hand)
+    elsif %w[King Queen Jack].include? value
+      total += 10
+    elsif value.is_a? Integer
+      total += value
+    end
+  end
+
+  total
+end
+
+def calc_totals(hands, skip_ace = false)
+  totals = { player: 0, dealer: 0 }
+
+  hands.each do |participant, hand|
+    totals[participant] = calc_hand(hand)
+  end
+
+  totals
+end
+
+# def display_hands(hands)
+
+# end
+
 loop do
   deck = create_deck
   hands = get_hands(deck)
+  p totals = calc_totals({:player=>[["Ace", "Hearts"], ["Queen", "Hearts"], ["Ace", 'por']], :dealer=>[[2, "Diamonds"], ['Ace', "Hearts"], [5, "Hearts"], ['Ace', "Hearts"]]})
   round_winner = nil
 
+  # display_hands(hands)
   break
 end
 
