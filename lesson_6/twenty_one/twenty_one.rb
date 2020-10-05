f41b9b5be9ee0efdac9f1bbed4d9a717f1154ec0
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-10-05 12:30:55 -0400

    add create_deck method

--- lesson_6/twenty_one/twenty_one.rb
+++ lesson_6/twenty_one/twenty_one.rb
@@ -53,6 +53,21 @@ draw_card(deck, hand):
   drawn_card = deck.sample
   hand << drawn_card
   deck.delete drawn_card
-
-
 =end
+
+SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+def create_deck
+  SUITS.each_with_object([]) do |suit_name, deck|
+    deck << 13.times.each_with_object([]) do |card_index, suit|
+      case card_index
+      when 12 then suit << ["Ace", suit_name]
+      when 11 then suit << ["King", suit_name]
+      when 10 then suit << ["Queen", suit_name]
+      when 9 then suit << ["Jack", suit_name]
+      else
+        if card_index < 9 then suit << [card_index + 2, suit_name] end 
+      end
+    end
+  end.flatten(1)
+end
